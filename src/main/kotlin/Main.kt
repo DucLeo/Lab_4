9dc77dd3ee6a30e440c9d162d6d8ca4f9e22afaf
DucLeo <75543799+DucLeo@users.noreply.github.com>
2022-05-05 19:48:12 +0300

    lab 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     println(
         "w,a,s,d - Move\n" +
-                "pause - Save game\n" +
+                "pause - Pause game\n" +
                 "exit - Quit game\n"
     )
     val model = Model()
